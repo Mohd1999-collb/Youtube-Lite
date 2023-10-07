bd87b9ae527afd0751144d031580abb3d311afa3
Mohd Talib <mohdsidd7232@gmail.com>
2023-10-07 17:31:12 +0530

    Youtube_lite done

---
+++ src/context/contextApi.js
@@ -0,0 +1,45 @@
+import React, { createContext, useState, useEffect } from "react";
+
+import { fetchDataFromApi } from "../utils/api";
+
+// Create the empty global context object;
+export const Context = createContext();
+
+// Store the value inside the global context object
+export const AppContext = (props) => {
+    const [loading, setLoading] = useState(false);
+    const [searchResults, setSearchResults] = useState([]);
+    const [selectedCategory, setSelectedCategory] = useState("New");
+    const [mobileMenu, setMobileMenu] = useState(false);
+
+    useEffect(() => {
+        fetchSelectedCategoryData(selectedCategory);
+    }, [selectedCategory]);
+
+    const fetchSelectedCategoryData = (query) => {
+        setLoading(true);
+        fetchDataFromApi(`search/?q=${query}`).then(({ contents }) => {
+            console.log(contents);
+            setSearchResults(contents);
+            setLoading(false);
+        }).catch((error)=> {
+            console.log(error.message)
+            alert("Daily api limit exceeded.")});
+    };
+
+    return (
+        <Context.Provider
+            value={{
+                loading,
+                setLoading,
+                searchResults,
+                selectedCategory,
+                setSelectedCategory,
+                mobileMenu,
+                setMobileMenu,
+            }}
+        >
+            {props.children}
+        </Context.Provider>
+    );
+};
